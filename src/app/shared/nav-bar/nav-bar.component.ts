6667e9ab732ae5b01d10778a5d9007ccbb076dbd
Daniel_Quiñonez <daniel.jose1999@outlook.com>
2024-06-10 21:21:59 -0500

    Changes Daniel Q

--- src/app/shared/nav-bar/nav-bar.component.ts
+++ src/app/shared/nav-bar/nav-bar.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class NavBarComponent {
 
+  isOpenNabVar: boolean = false;
+
+
+  openNavBar(){
+    this.isOpenNabVar = !this.isOpenNabVar;
+  }
+
+
 }
